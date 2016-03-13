dfd158a7b6a3d84040a21d1535d23ed9351cea10
Subtirelu Mihail <dazzzz2007@gmail.com>
2016-03-13 13:53:13 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# How to make magic
+
+### To install:
+
+`npm i`
+
+### To run:
+
+`npm start`
+
+### To build:
+
+`npm run build`
+
+
+## To do
+
+- add temp save to localstorage in case user does a refresh on the page
+- add code highlight to snippet form
+- add db validation rules
+- fix duplicate entries
+- add pagination
+- add db error handler
+- add keyboard save and submit
+- add support for [static page genrator](https://github.com/markdalgleish/static-site-generator-webpack-plugin)
+ 
+~~add validation to save form inputs~~
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var webpack = require('webpack');
+var path    = require('path');
+
+//Plugins
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var cssLoaderStr = [
+  "css-loader?sourceMap",
+  "postcss-loader",
+  "sass-loader" + '?sourceMap&outputStyle=expanded&sourceMap=true&sourceMapContents=true&includePaths[]='+ path.resolve(__dirname, 'node_modules/bulma')
+].join("!");
+
+var cssLoader = ExtractTextPlugin.extract("style", cssLoaderStr);
+
+module.exports = {
+    cache: true,
+    debug: true,
+    devtool: 'source-map',
+    entry: [
+      './src/js/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "bundle[hash].js",
+    },
+    resolve: {
+      root: [path.resolve('./src/js/'), path.resolve('./src/')],
+      extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            { test: path.resolve('./src/js'),  loader: 'babel-loader', exclude: /node_modules/},
+            { test: /\.s?css$/, loader: cssLoader },
+            { test: /\.(png|jpg|gif)$/, loader: 'url?limit=1048576' }, // inline base64 URLs for <=1MB images, direct URLs for the rest
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=image/svg+xml" }
+        ]
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html", 
+        inject: 'body'
+      }),
+      new ExtractTextPlugin("[contenthash].css"),
+      new webpack.DefinePlugin({
+        "process.env": {
+           NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+         }
+      }),
+    ]
+
+};
